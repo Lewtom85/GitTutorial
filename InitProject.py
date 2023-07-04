0e6852549ecda635fc7e62a0da6f9c836e1fdba3
Tomasz Lewandowski <lewtom85@gmail.com>
2023-07-04 23:30:38 +0200

    petla for

--- InitProject.py
+++ InitProject.py
@@ -2,3 +2,5 @@ lista_zakupow = {
 "piekarnia": ["chleb", "bulki", "paczki"],
 "warzywniak": ["marchew", "seler", "rukola"]
 }
+for klucz, wartosc in lista_zakupow.items():
+    print("Ide do sklepu:", (str(klucz).title()), "i kupuje:", (str(wartosc).title()))
