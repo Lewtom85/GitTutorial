157f96c5c4d2095232acec8b27076e26ba16e2f4
Tomasz Lewandowski <lewtom85@gmail.com>
2023-07-06 22:38:41 +0200

    Specjalne_Pozdrowienia

--- InitProject.py
+++ InitProject.py
@@ -2,7 +2,10 @@ lista_zakupow = {
 "piekarnia": ["chleb", "bulki", "paczki"],
 "warzywniak": ["marchew", "seler", "rukola"]
 }
+#Wprowadzenie petli
 for klucz, wartosc in lista_zakupow.items():
     print("Ide do sklepu:", (str(klucz).title()), "i kupuje:", (str(wartosc).title()))
+#Dodatkowa zmienna do obliczne
 suma = sum(len(v) for v in lista_zakupow.values())
-print("W sumie kupuje", suma, "produktow.") 
+print("W sumie kupuje", suma, "produktow.")
+print("SPECJALNE POZDROWIENIA DLA RAFALA Z MADRYTU :)") 
